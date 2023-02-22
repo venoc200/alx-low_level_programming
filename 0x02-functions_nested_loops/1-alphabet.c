054489e72880e4a1412e5a0254a37f5a708f7e21
venoc200 <ochubiri10@gmail.com>
2023-02-22 03:22:10 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - A function that prints the alphabets in lower case
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}

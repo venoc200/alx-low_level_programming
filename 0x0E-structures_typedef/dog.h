05a954fa838a9fc89de764f1ca4bdf1d7af0c60d
venoc200 <ochubiri10@gmail.com>
2023-03-21 04:19:16 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,11 +14,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
-
-/**
- * dog_t - typedef for struct dog
- */
+};
 
 typedef struct dog dog_t;
 
@@ -29,4 +25,4 @@ void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
-#endif
+#endif /*DOG_H*/
